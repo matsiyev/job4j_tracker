b58a7c78e68f29cb878b0cb63ec8286bbb5bfe9f
contrassst <mb_matsiyev@mail.ru>
2024-10-11 21:28:30 +0300

    3. Модели данных и HashSet. [#218530]

--- src/main/java/ru/job4j/collection/NotifyAccount.java
+++ src/main/java/ru/job4j/collection/NotifyAccount.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class NotifyAccount {
     public static HashSet<Account> sent(List<Account> accounts) {
-        return new HashSet<>(accounts);
+        HashSet<Account> accountCheck = new HashSet<>();
+        for(Account account : accounts) {
+            accountCheck.add(account);
+        }
+        return accountCheck;
     }
 }
