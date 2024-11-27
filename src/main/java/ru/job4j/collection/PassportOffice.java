f1766ebd3c6ba76048593759db8337fd33e461ec
contrasst <mb_matsiyev@mail.ru>
2024-11-27 21:38:40 +0300

    1. Паспорт и жители [#212656 #521740]

--- src/main/java/ru/job4j/collection/PassportOffice.java
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -8,7 +8,6 @@ public class PassportOffice {
     private Map<String, Citizen> citizens = new HashMap<>();
 
     public boolean add(Citizen citizen) {
-        boolean result = false;
         if (!(citizens.containsKey(citizen.getPassport()))) {
             citizens.put(citizen.getPassport(), citizen);
             return true;
